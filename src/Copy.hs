220212e1f23a65c02a4978781a6f4d4a03d4d715
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2016-03-10 12:49:32 -0800

    Update sync function

--- src/Copy.hs
+++ src/Copy.hs
@@ -14,27 +14,26 @@ data SyncInfo = SyncInfo {source::Text, destination::Text} deriving (Show)
 
 
 watch :: (SyncInfo -> IO()) -> SyncInfo -> IO ()
-watch f s = do
+watch fun syncInfo = do
   lock <- newMVar ()
   withManager $ \mgr -> do
-    putStrLn $ "SyncInfo: " ++  (show s)
+    putStrLn $ "SyncInfo: " ++  (show syncInfo)
     -- start a watching job (in the background)
     void $ watchTree
       mgr          -- manager
-      ((T.unpack.source) s)          -- directory to watch
+      ((T.unpack . source) syncInfo)          -- directory to watch
       (const True) -- predicate
       (\_-> void $ forkIO $ do
           took <- tryTakeMVar lock
           when (isJust took) $ do
             putStrLn "Change detected"
             threadDelay (1 * 1000 * 1000)
-            f s -- action
+            void $ fun syncInfo -- action
             putMVar lock ())
     -- sleep forever (until interrupted)
     forever $ threadDelay 1000000
 
-rsync :: SyncInfo -> Shell Text
-rsync syncInfo = do
-  (echo . T.pack . show) syncInfo
-  inproc "rsync" ["-arPvz", "--exclude", ".git", "--exclude","*.pyc", "--delete",
+--rsync :: SyncInfo -> Shell Text
+rsync syncInfo =
+  inproc "rsync" ["-arPvz", "--exclude", ".git", "--exclude", "*.pyc", "--delete",
                   (source syncInfo), (destination syncInfo)] empty
