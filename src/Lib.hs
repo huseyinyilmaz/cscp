493625112509a19c2e18e03e6da4104f0b2372a8
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2016-07-01 10:28:19 -0700

    Add many (Applicative) to exclude list.

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,19 +5,18 @@ import Turtle
 import qualified Data.Text as Text
 import Copy--(rsync, SyncInfo)
 
-parser :: Parser (Text, Text, Text)
+parser :: Parser (Text, Text, [Text])
 parser = (,,) <$> (argText "source" "source directory to copy")
               <*> (argText "destination" "destination directory to copy")
-              <*> (optText "exclude" 'e' "Exclude Following Paths(Coma separated List)")
+              <*> (many (optText "exclude" 'e' "Exclude Following Paths(Coma separated List)"))
 
 run :: IO ()
 run = do
-  (src, dst, rawEx) <- options
+  (src, dst, exs) <- options
     "Continuesly copies source directory to given destinatio directory"
     parser
-  let excludes =  Text.splitOn "," rawEx
   let syncInfo = SyncInfo{source=src,
                           destination=dst,
-                          excludes=excludes}
+                          excludes=exs}
   view $ rsync syncInfo
   watch (view.rsync) syncInfo
